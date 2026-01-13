622c7b400b4313fc7b097bd51a64b5c31e02d4a0
Deepanshu Garg <deepanshu.garg2@ibm.com>
2026-01-13 15:58:56 +0530

    New Release 2.31.254

--- docplex/version.py
+++ docplex/version.py
@@ -8,9 +8,9 @@
 # This file is generated !
 # See script tools/gen_version.py
 docplex_version_major = 2
-docplex_version_minor = 30
-docplex_version_micro = 251
-docplex_version_string = '2.30.251'
+docplex_version_minor = 31
+docplex_version_micro = 254
+docplex_version_string = '2.31.254'
 
 latest_cplex_major = 22
 latest_cplex_minor = 1
